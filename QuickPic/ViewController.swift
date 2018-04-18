b6d8ccaa773f8aa44b9116da4fa5b7d6da348b8a
Ben Chapman-Kish <ben.chapmankish@gmail.com>
2018-04-17 20:29:52 -0400

    Initial Commit

---
+++ QuickPic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickPic
+//
+//  Created by Ben Chapman-Kish on 2018-04-17.
+//  Copyright © 2018 Ben Chapman-Kish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
