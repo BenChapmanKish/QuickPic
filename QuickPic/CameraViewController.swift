cb6ae433c78a8dbd4b1d528979c95a3dfa130979
Ben Chapman-Kish <ben.chapmankish@gmail.com>
2018-04-22 03:59:05 -0400

    Add camera states, the ability to double tap to switch cameras, and the ability to exit the edit state by tapping the capture button again

--- QuickPic/CameraViewController.swift
+++ QuickPic/CameraViewController.swift
@@ -9,23 +9,62 @@
 import UIKit
 import AVFoundation
 
+enum CameraPageState: Equatable {
+    case livePreview(cameraPosition: AVCaptureDevice.Position)
+    case capturingImage(cameraPosition: AVCaptureDevice.Position)
+    case editing(capturedImage: UIImage)
+    
+    func cameraPosition() -> AVCaptureDevice.Position? {
+        switch self {
+        case .livePreview(let position),
+             .capturingImage(let position):
+            return position
+        default:
+            return nil
+        }
+    }
+    
+    func capturedImage() -> UIImage? {
+        switch self {
+        case .editing(let image):
+            return image
+        default:
+            return nil
+        }
+    }
+}
+
 class CameraViewController: UIViewController {
-    @IBOutlet var previewView: UIView!
+    @IBOutlet var livePreviewView: UIView!
     @IBOutlet var uiOverlayView: UIView!
     @IBOutlet var editsOverlayView: UIView!
+    @IBOutlet var capturedImageView: UIImageView!
+    
     @IBOutlet var captureButton: UIButton!
     
     var captureSession: AVCaptureSession?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
     var capturePhotoOutput: AVCapturePhotoOutput?
     
+    var state: CameraPageState = .livePreview(cameraPosition: .back)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupCamera()
+        self.setupSwitchCameraGesture()
+        self.setupCamera(forPosition: .back)
     }
     
-    func setupCamera() {
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+    func setupCamera(forPosition position: AVCaptureDevice.Position) {
+        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: position) else { return }
+        
+        self.state = .livePreview(cameraPosition: position)
+        
+        // teardown
+        self.captureSession?.stopRunning()
+        self.livePreviewView.layer.sublayers?.forEach {
+            $0.removeFromSuperlayer()
+        }
         
         var input: AVCaptureDeviceInput
         do {
@@ -38,10 +77,12 @@ class CameraViewController: UIViewController {
         let captureSession = AVCaptureSession()
         self.captureSession = captureSession
         
+        
         captureSession.addInput(input)
         
         
         let capturePhotoOutput = AVCapturePhotoOutput()
+        
         self.capturePhotoOutput = capturePhotoOutput
         
         captureSession.addOutput(capturePhotoOutput)
@@ -51,18 +92,51 @@ class CameraViewController: UIViewController {
         self.videoPreviewLayer = videoPreviewLayer
         videoPreviewLayer.videoGravity = .resizeAspectFill
         videoPreviewLayer.frame = self.view.layer.bounds
-        self.previewView.layer.insertSublayer(videoPreviewLayer, at: 0)
+        self.livePreviewView.layer.addSublayer(videoPreviewLayer)
         
         
         captureSession.startRunning()
+        
+        self.enterLivePreviewState(withPosition: position)
     }
     
+    private func setupSwitchCameraGesture() {
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(CameraViewController.handleTap(_:)))
+        tapGR.delegate = self
+        tapGR.numberOfTapsRequired = 2
+        self.uiOverlayView.addGestureRecognizer(tapGR)
+    }
+    
+    private func switchCameras() {
+        switch self.state {
+        case .livePreview(let cameraPosition):
+            if cameraPosition == .back {
+                self.setupCamera(forPosition: .front)
+            } else {
+                self.setupCamera(forPosition: .back)
+            }
+        default:
+            break
+        }
+    }
+    
+    
     @IBAction func onCaptureButtonTapped(_ sender: Any) {
-        self.captureImage()
+        switch self.state {
+        case .livePreview:
+            self.captureImage()
+        case .editing:
+            self.setupCamera(forPosition: .back)
+        default:
+            break
+        }
     }
     
     func captureImage() {
-        guard let capturePhotoOutput = self.capturePhotoOutput else { return }
+        guard let capturePhotoOutput = self.capturePhotoOutput,
+            let cameraPosition = self.state.cameraPosition() else { return }
+        
+        self.state = .capturingImage(cameraPosition: cameraPosition)
         
         let photoSettings = AVCapturePhotoSettings()
         photoSettings.flashMode = .auto
@@ -70,19 +144,51 @@ class CameraViewController: UIViewController {
         capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
     }
     
-    func drawEditsOn(image: UIImage) -> UIImage {
+    func drawEditsOnCapturedImage() -> UIImage? {
+        guard let capturedImage = self.state.capturedImage() else { return nil }
+        
         let layer = self.editsOverlayView.layer
         let scale = UIScreen.main.scale
         UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
-        guard let context = UIGraphicsGetCurrentContext() else { return image }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
         
-        image.draw(in: layer.frame)
+        capturedImage.draw(in: layer.frame)
         
         layer.render(in: context)
-        guard let screenshotImage = UIGraphicsGetImageFromCurrentImageContext() else { return image }
+        guard let editedImage = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
         UIGraphicsEndImageContext()
         
-        return screenshotImage
+        return editedImage
+    }
+    
+    func enterEditState(withImage image: UIImage, andCameraPosition position: AVCaptureDevice.Position) {
+        var flippedImage: UIImage?
+        if position == .front,
+            let cgimage = image.cgImage {
+            flippedImage = UIImage(cgImage: cgimage, scale: image.scale, orientation: .leftMirrored)
+        }
+        
+        
+        self.state = .editing(capturedImage: flippedImage ?? image)
+        self.editsOverlayView.isHidden = false
+        self.capturedImageView.isHidden = false
+        self.capturedImageView.image = flippedImage ?? image
+        self.captureSession?.stopRunning()
+        self.livePreviewView.isHidden = true
+    }
+    
+    func enterLivePreviewState(withPosition position: AVCaptureDevice.Position) {
+        self.state = .livePreview(cameraPosition: position)
+        self.editsOverlayView.isHidden = true
+        self.capturedImageView.isHidden = true
+        self.capturedImageView.image = nil
+        self.captureSession?.startRunning()
+        self.livePreviewView.isHidden = false
+    }
+    
+    func saveImageToCameraRoll() {
+        guard let editedImage = self.drawEditsOnCapturedImage() else { return }
+        UIImageWriteToSavedPhotosAlbum(editedImage, nil, nil, nil)
     }
 
     /*
@@ -98,11 +204,19 @@ class CameraViewController: UIViewController {
 
 extension CameraViewController : AVCapturePhotoCaptureDelegate {
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let imageData = photo.fileDataRepresentation(),
-        let capturedImage = UIImage.init(data: imageData) else { return }
-        
-        let screenshotImage = self.drawEditsOn(image: capturedImage)
-        
-        UIImageWriteToSavedPhotosAlbum(screenshotImage, nil, nil, nil)
+        switch self.state {
+        case .capturingImage(let position):
+            guard let imageData = photo.fileDataRepresentation(),
+                let capturedImage = UIImage.init(data: imageData) else { return }
+            self.enterEditState(withImage: capturedImage, andCameraPosition: position)
+        default:
+            self.setupCamera(forPosition: .back)
+        }
+    }
+}
+
+extension CameraViewController : UIGestureRecognizerDelegate {
+    @objc func handleTap(_ gesture: UITapGestureRecognizer){
+        self.switchCameras()
     }
 }
