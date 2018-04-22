70d7c8f186bbc25dd24abc47615d4f077d28a72e
Ben Chapman-Kish <ben.chapmankish@gmail.com>
2018-04-21 20:29:03 -0400

    Add capture button and save image + edits to file when it's pressed

--- QuickPic/CameraViewController.swift
+++ QuickPic/CameraViewController.swift
@@ -11,16 +11,17 @@ import AVFoundation
 
 class CameraViewController: UIViewController {
     @IBOutlet var previewView: UIView!
+    @IBOutlet var uiOverlayView: UIView!
+    @IBOutlet var editsOverlayView: UIView!
+    @IBOutlet var captureButton: UIButton!
     
     var captureSession: AVCaptureSession?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var capturePhotoOutput: AVCapturePhotoOutput?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.setupCamera()
-
-        // Do any additional setup after loading the view.
     }
     
     func setupCamera() {
@@ -39,20 +40,50 @@ class CameraViewController: UIViewController {
         
         captureSession.addInput(input)
         
+        
+        let capturePhotoOutput = AVCapturePhotoOutput()
+        self.capturePhotoOutput = capturePhotoOutput
+        
+        captureSession.addOutput(capturePhotoOutput)
+        
+        
         let videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         self.videoPreviewLayer = videoPreviewLayer
         videoPreviewLayer.videoGravity = .resizeAspectFill
-        videoPreviewLayer.frame = view.layer.bounds
-        self.previewView.layer.addSublayer(videoPreviewLayer)
+        videoPreviewLayer.frame = self.view.layer.bounds
+        self.previewView.layer.insertSublayer(videoPreviewLayer, at: 0)
+        
         
         captureSession.startRunning()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onCaptureButtonTapped(_ sender: Any) {
+        self.captureImage()
     }
     
+    func captureImage() {
+        guard let capturePhotoOutput = self.capturePhotoOutput else { return }
+        
+        let photoSettings = AVCapturePhotoSettings()
+        photoSettings.flashMode = .auto
+        
+        capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
+    }
+    
+    func drawEditsOn(image: UIImage) -> UIImage {
+        let layer = self.editsOverlayView.layer
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+        guard let context = UIGraphicsGetCurrentContext() else { return image }
+        
+        image.draw(in: layer.frame)
+        
+        layer.render(in: context)
+        guard let screenshotImage = UIGraphicsGetImageFromCurrentImageContext() else { return image }
+        UIGraphicsEndImageContext()
+        
+        return screenshotImage
+    }
 
     /*
     // MARK: - Navigation
@@ -63,5 +94,15 @@ class CameraViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension CameraViewController : AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let imageData = photo.fileDataRepresentation(),
+        let capturedImage = UIImage.init(data: imageData) else { return }
+        
+        let screenshotImage = self.drawEditsOn(image: capturedImage)
+        
+        UIImageWriteToSavedPhotosAlbum(screenshotImage, nil, nil, nil)
+    }
 }
